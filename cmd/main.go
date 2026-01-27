32dba4045be228f5683fb1ec1abd634b3d28b08d
Kosench <ilyakosenko09@gmail.com>
2026-01-27 22:41:58 +1000

    feat: server with graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,61 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Kosench/ecommerce-lab/internal/handler"
+	"github.com/Kosench/ecommerce-lab/internal/repository"
+	"github.com/Kosench/ecommerce-lab/internal/service"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 func main() {
-	log.Println("Order service starting...")
-	// TODO: инициализация сервера
+	dbURL := "postgres://user:pass@localhost:5432/orderdb?sslmode=disable"
+	httpAddr := ":8080"
+
+	pool, err := pgxpool.New(context.Background(), dbURL)
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	defer pool.Close()
+
+	orderRepo := repository.NewOrderRepository(pool)
+	orderService := service.NewOrderService(orderRepo)
+	orderHandler := handler.NewOrderHandler(orderService)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /orders", orderHandler.CreateOrder)
+
+	server := http.Server{
+		Addr:    httpAddr,
+		Handler: mux,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("server starting on %s", httpAddr)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("server failed: %v", err)
+		}
+	}()
+
+	<-done
+	log.Println("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown failed: %v", err)
+	}
+
+	log.Println("server stopped")
 }
